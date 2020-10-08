00a76ffa264f204fb4def4e871291714deb0ec7e
Lucas Mendes <lucasmendes.lx@gmail.com>
2020-10-08 19:55:52 -0300

    change PORT config

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import routes from './routes';
 import './database';
 
 const app = express();
-const PORT : string|number = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
 
 dotenv.config();
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
   console.log('🏃 Server Online');
 });
